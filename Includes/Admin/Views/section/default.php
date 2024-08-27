ef9d822c034a4cd6c51dfed461c104db59a3f685
sirajiwpdev <sirajiwpdev@gmail.com>
2024-08-27 15:21:45 +0600

    initial release 1.0.0

--- Includes/Admin/Views/section/default.php
+++
@@ -1,3 +0,0 @@
-<?php 
-include __DIR__ . '/banner.php';
-include __DIR__ . '/feature-section.php'; ?>
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c0a0dfac6be75bc4e942');
